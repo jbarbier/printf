c836cf98e5ebfbbdbabca9f834065bae052ce2b0
Jennie Chu <jenniechu92@gmail.com>
2016-11-22 00:14:36 +0000

    add header

--- holberton.h
+++ holberton.h
@@ -1,5 +1,6 @@
 #ifndef HEADER
 #define HEADER
 int no_conversion(char);
-char *split_format(char *format);
+char **split_format(char *format);
+char **fill_format(char *format, char *format_array[]);
 #endif
