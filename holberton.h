71cff7472a0a4a757ee151ddf5cfeddd80c40b9d
Anne <anne.cognet@holbertonschool.com>
2016-11-22 00:23:37 +0000

    adding structs and checks for c and s

--- holberton.h
+++ holberton.h
@@ -2,4 +2,44 @@
 #define HEADER
 int no_conversion(char);
 char *split_format(char *format);
+
+/*in buffer_functions1.c*/
+char *flush(char *buffer);
+char *fill_buffer(char *buffer, char *s, int count_c, int s_length);
+void print_buffer(char *buffer, int length);
+
+/*in conversion_checks1.c*/
+int conversion_char(char *s);
+int conversion_string(char *s);
+
+
+/*structs*/
+/**
+ * struct valid - check if formatting is valid
+ * @type
+ * @conversion_check - function to check if a conversion string is valid
+ */
+typedef struct valid
+{
+	char type;
+	int (*conversion_check)(char *);
+} valid;
+
+/**
+ *struct m_string - make strinf from valid format
+ * @type: type of format
+ * @m_string - make the string
+ */
+typedef struct m_string
+{
+	char type;
+	char *(*make_s)(char *);
+} m_string;
+
+
+
+
+
+
+
 #endif
